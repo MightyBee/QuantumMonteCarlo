222a7ab155adb17de7e732dfe83a9ac6bca7486e
Theo Saporiti <theosaporiti@gmail.com>
2019-11-13 23:36:09 +0000

    Replaced spaces with tabs (matlab)

--- test.m
+++ test.m
@@ -6,16 +6,16 @@ t=plot([data(1,:) data(1,1)],[0:1:size(data,2)]);
 xlim([-10 10])
 xlabel('x')
 ylabel('\tau / \delta \tau')
-grid on 
+grid on
 closw=waitforbuttonpress;
 
 
 for i=2:size(data,1)
-    pause(.01)
-    if ~ishandle(t)
-        break % Arrete l'animation si la fenetre est fermee
-    end
-    set(t,'XData',[data(i,:) data(i,1)])
+		pause(.01)
+		if ~ishandle(t)
+				break % Arrete l'animation si la fenetre est fermee
+		end
+		set(t,'XData',[data(i,:) data(i,1)])
 end
 %}
 
@@ -23,7 +23,7 @@ end
 n_part=2;
 data=load("output.out");
 %for k=1:n_part
-%   A(:,:,k)=data( 
+%	A(:,:,k)=data(
 %end
 figure
 t=histogram(data(1,:),91,'Normalization','probability');
@@ -33,9 +33,9 @@ closw=waitforbuttonpress;
 
 
 for i=2:size(data,1)
-    pause(.01)
-    if ~ishandle(t)
-        break % Arrete l'animation si la fenetre est fermee
-    end
-    set(t,'Data',data(i,:))
+	pause(.01)
+		if ~ishandle(t)
+				break % Arrete l'animation si la fenetre est fermee
+		end
+		set(t,'Data',data(i,:))
 end
