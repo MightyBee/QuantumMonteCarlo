f58a91df3d1a10a5e21329b38755017e5834bb21
MightyBee <37081437+MightyBee@users.noreply.github.com>
2019-11-16 19:02:50 +0000

    3d updates for main.cc

--- SRC/main.cc
+++ SRC/main.cc
@@ -1,4 +1,5 @@
 #include <vector>
+#include <array>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -25,28 +26,28 @@ int main(int argc, char* argv[]){
                N_slices(configFile.get<unsigned int>("N_slices")); // number of (imaginary) time slices
   double tau_final(configFile.get<double>("tau")); // tau<->beta=1/(T*k_B) the so called inverse temperature
 	double d_tau(tau_final/N_slices); // imaginary time step
-	double m(configFile.get<double>("mass")*d_tau), // dimensionless mass
-         omega(configFile.get<double>("frequency")*d_tau); // dimensionless angular frequency
-  double pos_min(configFile.get<double>("pos_min")), // initial mininum position
-         pos_max(configFile.get<double>("pos_max")); // initial maximal position
-  double h(configFile.get<double>("h")); // initial maximum displacement of a point in the path
+	double m(configFile.get<double>("mass")*d_tau); // dimensionless mass
+  array<double,3> omega2({pow(configFile.get<double>("frequency_x")*d_tau,2),pow(configFile.get<double>("frequency_y")*d_tau,2),pow(configFile.get<double>("frequency_z")*d_tau,2)}); // dimensionless angular frequency
+  array<double,3> pos_min({configFile.get<double>("pos_min_x"),configFile.get<double>("pos_min_y"),configFile.get<double>("pos_min_z")}), // initial mininum position
+                  pos_max({configFile.get<double>("pos_max_x"),configFile.get<double>("pos_max_y"),configFile.get<double>("pos_max_z")}); // initial maximal position
+  array<double,3> h({configFile.get<double>("h_x"),configFile.get<double>("h_y"),configFile.get<double>("h_z")}); // initial maximum displacement of a point in the path
   double accrate(0.0), idrate(configFile.get<double>("idrate"));
-	vector<vector<double>> system(N_part,vector<double>(N_slices,0.0)); // table : each row corresponds to a particle and contains the coordinates (1D for now) in different time slices
+	vector<vector<array<double,3>>> system(N_part,vector<array<double,3>>(N_slices)); // table : each row corresponds to a particle and contains the coordinates (1D for now) in different time slices
 	vector<vector<double>> verif(N_part,vector<double>(N_slices,0.0));
 
-	int lol_test(0);
 	for(auto& particle : system){ // initialize random paths for each particles
 		for(auto& pos : particle){
-			pos=randomDouble(pos_min+lol_test,pos_max+lol_test);
+			for(size_t i(0); i<3; i++){
+				pos[i]=randomDouble(pos_min[i],pos_max[i]);
+			}
 		}
-		lol_test+=3;
 	}
 
 	unsigned int MCS(configFile.get<unsigned int>("MCS")); // number of Monte Carlo iterations
   size_t n_stride(configFile.get<size_t>("n_stride")); // output is written in the output file every n_stride iterations
 	unsigned int mm(0), mm_plu(0), mm_min(0); // mm : time slice randomly selected during each iteration, mm_plu=mm+1, mm_min=mm-1;
 	unsigned int nn(0); // particle randomly selected during each iteration
-	double new_r(0.0); // new position proposed
+	array<double,3> new_r({0.0,0.0,0.0}); // new position proposed
 	double s_old(0.0), s_new(0.0); // part of the action that is changed with the new position proposed (old and new values respectively)
 
 	string output("output.out"); // output file
@@ -62,35 +63,49 @@ int main(int argc, char* argv[]){
 				mm_min=(mm+1)%N_slices; // mm+1 with periodic boundary condition
 				nn=rand()%N_part; // random integer between 0 and N_part-1
 				verif[nn][mm]++;
-				new_r=system[nn][mm]+randomDouble(-1,1)*h; // proposed new position for the particle nn at time slice mm*d_tau
-
+				for(size_t i(0); i<3; i++){
+					new_r[i]=system[nn][mm][i]+randomDouble(-1,1)*h[i]; // proposed new position for the particle nn at time slice mm*d_tau
+				}
 				// as we take the difference of new and old action S_new-S_old, we can consider only the part of the action that is affected by the proposed new position
-				s_old=0.5*pow(system[nn][mm_plu]-system[nn][mm],2)
-				     +0.5*pow(system[nn][mm]-system[nn][mm_min],2)
-				     //+0.5*pow(system[nn][mm],2); // harmonic potential
-						 +0.1*pow(system[nn][mm],4)
-						 -1.0*pow(system[nn][mm],2);
-				s_new=0.5*pow(system[nn][mm_plu]-new_r,2)
-				     +0.5*pow(new_r-system[nn][mm_min],2)
-				     //+0.5*pow(new_r,2);
-						 +0.1*pow(new_r,4)
-						 -1.0*pow(new_r,2);
+				s_old=0.0;
+				for(size_t i(0); i<3; i++){
+					s_old+=0.5*pow(system[nn][mm_plu][i]-system[nn][mm][i],2)
+					      +0.5*pow(system[nn][mm][i]-system[nn][mm_min][i],2);
+				}
+				for(size_t i(0); i<3; i++){
+					s_old+=0.5*omega2[i]*pow(system[nn][mm][i],2); // harmonic potential
+				}
+
+				s_new=0.0;
+				for(size_t i(0); i<3; i++){
+					s_new+=0.5*pow(system[nn][mm_plu][i]-new_r[i],2)
+					      +0.5*pow(new_r[i]-system[nn][mm_min][i],2);
+				}
+				for(size_t i(0); i<3; i++){
+					s_new+=0.5*omega2[i]*pow(new_r[i],2); // harmonic potential
+				}
 
 				if(randomDouble(0,1)<exp(s_old-s_new)){ // metropolis acceptance
 					system[nn][mm]=new_r;
 	        accrate+=1.0/N_slices;
 				}
 			}
-			if(i>500) h*=accrate/idrate;
-			//cout << accrate << " " << h << endl;
-			accrate=0.0;
 		}
+		/*if(i>500){
+			for(size_t k(0); k<3; k++){
+				h[i]*=accrate/idrate;
+			}
+		}
+		cout << accrate << " " << h[1] << " " << h[2] << " " << h[3] << endl;
+		accrate=0.0;*/
 
 		// writing in the output file
 		if(!(i%n_stride)){
-			for(auto& particle : system){
-				for(auto& pos : particle){
-					fichier_output << pos << " ";
+			for(const auto& particle : system){
+				for(const auto& pos : particle){
+					for(size_t i(0); i<3; i++){
+						fichier_output << pos[i] << " ";
+					}
 				}
 			}
 			fichier_output << endl;
